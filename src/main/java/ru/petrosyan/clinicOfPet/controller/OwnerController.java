0302b9f56abf82f1f2fd45bc56d6b1d6e2ea7275
armen <armenpetrosan319@gmail.com>
2026-01-25 17:30:42 +0300

    fetch: Добавленение полностью реализовано и работает

--- src/main/java/ru/petrosyan/clinicOfPet/controller/OwnerController.java
+++ src/main/java/ru/petrosyan/clinicOfPet/controller/OwnerController.java
@@ -3,13 +3,14 @@ package ru.petrosyan.clinicOfPet.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ru.petrosyan.clinicOfPet.dao.OwnerDAO;
 import ru.petrosyan.clinicOfPet.model.Owner;
+import ru.petrosyan.clinicOfPet.utils.OwnerValidator;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -17,10 +18,12 @@ import java.util.List;
 public class OwnerController {
 
     private final OwnerDAO ownerDAO;
+    private final OwnerValidator ownerValidator;
 
     @Autowired
-    public OwnerController(OwnerDAO ownerDAO) {
+    public OwnerController(OwnerDAO ownerDAO, OwnerValidator ownerValidator) {
         this.ownerDAO = ownerDAO;
+        this.ownerValidator = ownerValidator;
     }
 
     @GetMapping
@@ -45,5 +48,15 @@ public class OwnerController {
     public String getFormInsert(Model model) {
         model.addAttribute("owner", new Owner());
         return "owner/newOwner";
+    }
+
+    @PostMapping
+    public String insertOwner(@ModelAttribute("owner") @Valid Owner owner, BindingResult bindingResult) {
+        ownerValidator.validate(owner, bindingResult);
+        if (bindingResult.hasErrors()) {
+            return "owner/newOwner";
+        }
+        ownerDAO.insertOwner(owner);
+        return "redirect:/owner";
     }
 }

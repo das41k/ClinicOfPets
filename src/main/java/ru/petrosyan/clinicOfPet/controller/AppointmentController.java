a979d907a9808ca6dc4f7a5f7891ab572326d2e9
armen <armenpetrosan319@gmail.com>
2026-01-29 14:29:00 +0300

    fetch(insert): Реализована форма для добавления

--- src/main/java/ru/petrosyan/clinicOfPet/controller/AppointmentController.java
+++ src/main/java/ru/petrosyan/clinicOfPet/controller/AppointmentController.java
@@ -1,5 +1,6 @@
 package ru.petrosyan.clinicOfPet.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ru.petrosyan.clinicOfPet.dao.AppointmentDAO;
+import ru.petrosyan.clinicOfPet.dao.PetDAO;
+import ru.petrosyan.clinicOfPet.dao.VeterinarianDAO;
 import ru.petrosyan.clinicOfPet.model.Appointment;
+import ru.petrosyan.clinicOfPet.model.Pet;
+import ru.petrosyan.clinicOfPet.model.Veterinarian;
+import ru.petrosyan.clinicOfPet.utils.AppointmentValidator;
+
 import java.util.List;
 
 @Controller
@@ -15,9 +22,16 @@ import java.util.List;
 public class AppointmentController {
 
     private final AppointmentDAO appointmentDAO;
+    private final AppointmentValidator appointmentValidator;
+    private final VeterinarianDAO veterinarianDAO;
+    private final PetDAO petDAO;
 
-    public AppointmentController(AppointmentDAO appointmentDAO) {
+    @Autowired
+    public AppointmentController(AppointmentDAO appointmentDAO, AppointmentValidator appointmentValidator, VeterinarianDAO veterinarianDAO, PetDAO petDAO) {
         this.appointmentDAO = appointmentDAO;
+        this.appointmentValidator = appointmentValidator;
+        this.veterinarianDAO = veterinarianDAO;
+        this.petDAO = petDAO;
     }
 
     @GetMapping
@@ -36,5 +50,19 @@ public class AppointmentController {
         }
         redirectAttributes.addFlashAttribute("error", "Данный прием не найден! Возможно он был удален!");
         return "redirect:/appointment";
+    }
+
+    @GetMapping("/new")
+    public String getFormInsert(Model model) {
+        model.addAttribute("appointment", new Appointment());
+        getDetailsForm(model);
+        return "appointment/addAppointment";
+    }
+
+    public void getDetailsForm(Model model) {
+        List<Veterinarian> veterinarians = veterinarianDAO.getAllVeterinarians();
+        List<Pet> pets = petDAO.getAllPets();
+        model.addAttribute("veterinarians", veterinarians);
+        model.addAttribute("pets", pets);
     }
 }

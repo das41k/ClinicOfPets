ded56f974f0e92780fceb7bd7a2d9ea642ed647f
armen <armenpetrosan319@gmail.com>
2026-01-29 13:26:17 +0300

    fetch: Просмотр сведений по id полностью работает

--- src/main/java/ru/petrosyan/clinicOfPet/controller/AppointmentController.java
+++ src/main/java/ru/petrosyan/clinicOfPet/controller/AppointmentController.java
@@ -3,7 +3,9 @@ package ru.petrosyan.clinicOfPet.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ru.petrosyan.clinicOfPet.dao.AppointmentDAO;
 import ru.petrosyan.clinicOfPet.model.Appointment;
 import java.util.List;
@@ -23,5 +25,16 @@ public class AppointmentController {
         List<Appointment> appointments = appointmentDAO.getAllAppointments();
         model.addAttribute("appointments", appointments);
         return "appointment/allAppointments";
+    }
+
+    @GetMapping("/{id}")
+    public String getAppointmentById(@PathVariable("id") Integer id, Model model, RedirectAttributes redirectAttributes) {
+        Appointment appointment = appointmentDAO.getByIdAppointment(id);
+        if (appointment != null) {
+            model.addAttribute("appointment", appointment);
+            return "appointment/appointment";
+        }
+        redirectAttributes.addFlashAttribute("error", "Данный прием не найден! Возможно он был удален!");
+        return "redirect:/appointment";
     }
 }

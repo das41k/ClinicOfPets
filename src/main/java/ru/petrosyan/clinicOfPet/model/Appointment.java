b584e2ea4dd03a44343904eec89869212775ff16
armen <armenpetrosan319@gmail.com>
2026-01-29 12:30:32 +0300

    fetch: Реализовал метож получения всех приемов в AppointmentDAO

--- src/main/java/ru/petrosyan/clinicOfPet/model/Appointment.java
+++ src/main/java/ru/petrosyan/clinicOfPet/model/Appointment.java
@@ -16,4 +16,63 @@ public class Appointment {
     private Veterinarian veterinarian;
     private Pet pet;
     private AppointmentStatus status;
+
+    public Appointment() {}
+
+    public Appointment(Integer appointment_id, LocalDate dateAdmission, LocalTime timeAdmission, Veterinarian veterinarian, Pet pet, AppointmentStatus status) {
+        this.appointment_id = appointment_id;
+        this.dateAdmission = dateAdmission;
+        this.timeAdmission = timeAdmission;
+        this.veterinarian = veterinarian;
+        this.pet = pet;
+        this.status = status;
+    }
+
+    public Integer getAppointment_id() {
+        return appointment_id;
+    }
+
+    public LocalDate getDateAdmission() {
+        return dateAdmission;
+    }
+
+    public LocalTime getTimeAdmission() {
+        return timeAdmission;
+    }
+
+    public Veterinarian getVeterinarian() {
+        return veterinarian;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public AppointmentStatus getStatus() {
+        return status;
+    }
+
+    public void setAppointment_id(Integer appointment_id) {
+        this.appointment_id = appointment_id;
+    }
+
+    public void setDateAdmission(LocalDate dateAdmission) {
+        this.dateAdmission = dateAdmission;
+    }
+
+    public void setTimeAdmission(LocalTime timeAdmission) {
+        this.timeAdmission = timeAdmission;
+    }
+
+    public void setVeterinarian(Veterinarian veterinarian) {
+        this.veterinarian = veterinarian;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public void setStatus(AppointmentStatus status) {
+        this.status = status;
+    }
 }

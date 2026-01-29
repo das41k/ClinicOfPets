d8944c83272c33633f1070d1fe2e6d268aa6e090
armen <armenpetrosan319@gmail.com>
2026-01-29 15:07:21 +0300

    feat: реализовал метод для обновления в AppointmentDAO

--- src/main/java/ru/petrosyan/clinicOfPet/dao/AppointmentDAO.java
+++ src/main/java/ru/petrosyan/clinicOfPet/dao/AppointmentDAO.java
@@ -122,4 +122,28 @@ public class AppointmentDAO {
             e.printStackTrace();
         }
     }
+
+    public void updateAppointment(Appointment appointment) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("update appointment set " +
+                     "date_admission = ?, time_admission = ?, veterinarian_id = ?, pet_id = ? where appointment_id = ?")
+        ) {
+            preparedStatement.setDate(1, Date.valueOf(appointment.getDateAdmission()));
+            preparedStatement.setTime(2, Time.valueOf(appointment.getTimeAdmission()));
+            preparedStatement.setInt(3, appointment.getVeterinarian().getVeterinarian_id());
+            preparedStatement.setInt(4, appointment.getPet().getPet_id());
+            preparedStatement.setInt(5, appointment.getAppointment_id());
+
+            int row = preparedStatement.executeUpdate();
+            if (row > 0) {
+                System.out.println("Update by appointment is success");
+            } else {
+                System.out.println("Update by appointment is unsuccess");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
 }
